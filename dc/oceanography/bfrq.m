6ed8d0c09b8ce929b1dcd8a43c8d621af813e849
deepak <dcherian@mit.edu>
2016-09-24 12:18:34 -0400

    bfrq is improved.
    Use interp1 nearest-neighbour extrapolation to fill in NaNs with last
    known good values. Doesn't clutter up output.

--- dc/oceanography/bfrq.m
+++ dc/oceanography/bfrq.m
@@ -7,24 +7,18 @@ function [N2] = bfrq(s,t,z,lat)
     %           lat -> latitude
 
     lz = length(z);
-	N2 = (-9.81./sw_smow(t(1:lz-1))).*ones(lz-1,1);
-	pref = sw_pres((z(1:lz-1)+z(2:lz))/2,lat);
+    N2 = (-9.81./sw_smow(t(1:lz-1))).*ones(lz-1,1);
+    pref = sw_pres((z(1:lz-1)+z(2:lz))/2,lat);
     p = sw_pres(z,lat);
-    
-	for k=1:lz-1
-        N2(k) = N2(k)*(sw_pden(s(k),t(k),p(k),pref(k))-sw_pden(s(k+1),t(k+1),p(k+1),pref(k)))/(z(k+1)-z(k));
-	end
-    
-    % Account for missing values in T at large depths
-    ind = sort(find(isnan(N2) == 1));
-    if isempty(ind)
-        return
-    else
-        fprintf('\n NaN detected in N2.');
-        %if ind(1) >= lz-2
-            fprintf(' Filling %d values with nearest value @ depth %d', length(ind),ind(1)-1);
-            ind
-            N2(ind) = N2(ind(1)-1);
-            % end
+
+    for k=1:lz-1
+        N2(k) = N2(k)*(sw_pden(s(k),t(k),p(k),pref(k)) ...
+                       - sw_pden(s(k+1),t(k+1),p(k+1),pref(k)))/(z(k+1)-z(k));
     end
-    %end
+
+    % fill in NaNs with nearest value.
+    nans = isnan(N2);
+    zn2 = avg1(z);
+    N2(nans) = ...
+        interp1(zn2(~nans), N2(~nans), zn2(nans), 'nearest', 'extrap');
+end
