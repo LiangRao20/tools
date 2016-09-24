144d9c9c4a50297e523ff72195cdbe0726d56ef0
deepak <dcherian@mit.edu>
2016-09-24 11:42:13 -0400

    Massively speed up calcdof.
    Avoid calls to trapz with a summation (spacing is unity).

--- dc/time series/calcdof.m
+++ dc/time series/calcdof.m
@@ -4,13 +4,14 @@ function [dof, IntegralTimeScale] = calcdof(in)
 
     [c,lags] = xcorr(in - mean(in), 'coef');
     [cmax,imax] = max(c);
-    i0 = imax + find(c(imax:end) < 0, 1, 'first') - 1; % first zero crossing
+    % i0 = imax + find(c(imax:end) < 0, 1, 'first') - 1; % first zero crossing
 
     % calculate a bunch of timescales and take maximum
     % From Talley et al., Descriptive Physical Oceanography.
+    IntegralTimeScale = nan([1 length(c) - (imax + 3) + 1]);
     for tt=imax+3:length(c)
         ff = tt - (imax+3) + 1;
-        CorrArea = trapz(c(imax:tt));
+        CorrArea = sum( (c(imax:tt-1) + c(imax+1:tt))/2 );
         IntegralTimeScale(ff) = CorrArea/cmax;
     end
 
