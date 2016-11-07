a964a33af4e692103909d4ba78f620cf7463b710
deepak <dcherian@mit.edu>
2016-11-07 17:33:46 -0500

    Don't modify axes in resizeImageForPub.
    Some extra options.

--- dc/plots/resizeImageForPub.m
+++ dc/plots/resizeImageForPub.m
@@ -7,15 +7,10 @@ function [] = resizeImageForPub(option)
     end
 
     hfig = gcf;
-    hax = gca;
     funits = hfig.Units;
-    aunits = hax.Units;
 
     hfig.Units = 'inches';
     hfig.Resize = 'off'; % for some reason required.
-
-    hax.Units = 'normalized';
-    hax.OuterPosition = [0 0 1 1];
 
     if strcmpi(option, 'onecolumn')
         hfig.Position(3) = (8.5-1)/2;
@@ -25,13 +20,27 @@ function [] = resizeImageForPub(option)
         hfig.Position(3) = 8.5-1;
     end
 
+    if strcmpi(option, 'onecolumnfullpage')
+        hfig.Position(3) = (8.5-1)/2;
+        hfig.Position(4) = 11.5-2;
+        hfig.Units = 'normalized';
+        hfig.Position(2) = 0.9 - hfig.Position(4);
+    end
+
+    if strcmpi(option, 'portraitfullpage')
+        hfig.Position(3) = 8.5-1;
+        hfig.Position(4) = 11.5-2;
+        hfig.Units = 'normalized';
+        hfig.Position(2) = 0.9 - hfig.Position(4);
+    end
+
     if strcmpi(option, 'landscape')
         hfig.Position(3) = 11.5-1;
     end
 
     hfig.Units = funits;
-    hax.Units = aunits;
-    pause(1);hfig.Resize = 'on';
+    pause(1);
+    hfig.Resize = 'on';
 end
 
 function test_resize
