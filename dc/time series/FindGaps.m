0135189b4adb69c3b958157670c1ca4cb4238d85
deepak <dpak.cherian@gmail.com>
2016-12-02 13:53:31 -0500

    FindGaps: Make sure gapstart and gapend are of same size.

--- dc/time series/FindGaps.m
+++ dc/time series/FindGaps.m
@@ -1,18 +1,26 @@
 % Returns start and end inidices of gaps in input time series.
 %    [gapstart, gapend] = FindGaps(in)
 function [gapstart, gapend] = FindGaps(in)
+
     nans = isnan(in);
     edges = diff(nans);
     gapstart = find(edges == 1) + 1;
     gapend = find(edges == -1);
 
-    if isnan(in(1)), gapstart = [1 gapstart]; end
+    if isnan(in(1))
+        gapstart(2:end+1) = gapstart;
+        gapstart(1) = 1;
+    end
     if isnan(in(end))
         gapend(end+1) = length(in);
     else
         % last gap is not at the end of time series.
         gapstart(end+1) = length(in)+1;
         gapend(end+1) = length(in)+1;
+    end
+
+    if ~isequal(size(gapstart), size(gapend))
+        gapend = gapend';
     end
 
     if isempty(gapstart) & isempty(gapend) ...
@@ -22,6 +30,6 @@ function [gapstart, gapend] = FindGaps(in)
         gapend = gapstart;
     end
 
-    assert(length(gapstart) == length(gapend), ...
+    assert(isequal(size(gapstart), size(gapend)), ...
            ['FindGaps: gapstart and gapend are not same size.']);
 end
