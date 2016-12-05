13f50a289888a5ec90515a4a7c3481647a688524
deepak <dpak.cherian@gmail.com>
2016-12-05 17:18:59 -0500

    Modify Tom's code to use matrix inputs.
    Modified centeredFFT and synthetic_timeseries_known_spectrum.
    
    First dimension is time.

--- tom/centeredFFT.m
+++ tom/centeredFFT.m
@@ -6,14 +6,18 @@ function [X,freq]=centeredFFT(x,dt)
 %
 % Inputs:
 %    x, vector to be transformed
+%      if matrix, FFT along first dimension.
 %    dt, time increment
 %
 % Adapted from a function written by Quan Quach of blinkdagger.com 
 % Tom Farrar, 2016, jfarrar@whoi.edu
 
+if size(x,1) == 1
+    x = x';
+end
 
+N=size(x,1);
 
-N=length(x);
 %Generate frequency index
 if mod(N,2)==0
     m=-N/2:N/2-1; % N even
@@ -21,6 +25,7 @@ else
     m=-(N-1)/2:(N-1)/2; % N odd
 end
 freq=m/(N*dt);  %the dimensional frequency scale
-X=fft(x);
-X=fftshift(X); %swaps the halves of the FFT vector so that the zero frequency is in the center\\
-%If you are going to compute an IFFT, first use X=ifftshift(X) to undo the shift}
+X=fft(x, [], 1);
+X=fftshift(X, 1);
+%swaps the halves of the FFT vector so that the zero frequency is in the center
+%If you are going to compute an IFFT, first use X=ifftshift(X) to undo the shift
--- tom/synthetic_timeseries_known_spectrum.m
+++ tom/synthetic_timeseries_known_spectrum.m
@@ -4,14 +4,14 @@
 %  power-law spectrum, S=alpha*freq^Beta.
 %
 %Inputs:
-%  N, number of points
+%  N, number of points; if 2 elements, N = [number-of-time-instants number-of-timeseries]
 %  dt, sampling interval (in desired units)
 %  alpha, multiplicative scaling factor for spectrum (S=alpha*freq^Beta)
 %  Beta, power-law dependence, such that spectrum is proportional to freq^-Beta
 %
 %Output:
 %  yfilt, a  time series with the specified power-law dependence
-%  
+%
 %
 % This function is derived from synthestic_timeseries.m, which had a unit-variance output
 % time series.  Here, the time series variance is whatever is implied by the spectrum
@@ -23,23 +23,27 @@
 
 function [yfilt]=synthetic_timeseries_known_spectrum(N,dt,alpha,beta);
 
-gy=randn(N,1);
-y=y(:);
+if length(N) > 1
+    y = randn(N);
+else
+    y=randn(N,1);
+end
 
 [Y,freq]=centeredFFT(y,dt);
 T=N.*dt;
 
+freq = repmat(freq', [1 size(y, 2)]);
 %Yfilt=sqrt(alpha).*N./sqrt(2.*T).*abs(freq').^(beta./2).*Y;
 %Yfilt=sqrt(alpha).*sqrt(N./(2.*dt)).*abs(freq').^(beta./2).*Y;
-Yfilt=sqrt(alpha).*sqrt(1./(2.*dt)).*abs(freq').^(beta./2).*Y;
+Yfilt=sqrt(alpha).*sqrt(1./(2.*dt)).*abs(freq).^(beta./2).*Y;
 %Yfilt=sqrt(alpha).*abs(freq').^(beta./2).*Y;
 
-ff=find(freq==0);Yfilt(ff)=0;
-Yfilt2=ifftshift(Yfilt);
-yfilt=ifft(Yfilt2);
+ff=find(freq(:,1)==0);
+Yfilt(ff,:)=0;
+Yfilt2=ifftshift(Yfilt, 1);
+yfilt=ifft(Yfilt2, [], 1);
 
 %Lines below confirm that fftshift, fft, and inverses all work as expected and give original noise timeseries:
 %Ytest=ifftshift(Y);
 %ytest=ifft(Ytest);
 %max_ytest_error=max(y-ytest)
-
