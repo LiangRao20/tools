12b7ef26a075175a4cb77120398617b5e9506bd2
deepak <dpak.cherian@gmail.com>
2016-12-08 14:10:27 -0500

    dcregress: Check on error calculations
    Check Wunsch's matrix formulation from standard slope error equation.
    It works! but add assertion just to be sure.

--- dc/time series/dcregress.m
+++ dc/time series/dcregress.m
@@ -1,13 +1,18 @@
 % Does Ordinary least squares. Calculates confidence bounds after
 % estimating degrees of freedom.
-%    [coeff,conf,dof] = dcregress(x, y, dof, test_flag, plot_flag, err_flag)
-function [coeff,conf,dof] = dcregress(x, y, dof, test_flag, plot_flag, ...
-                                      err_flag)
+%    [coeff,conf,dof,err] = dcregress(x, y, dof, test_flag, plot_flag, dof_flag)
+% coeff = [intercept slope]
+% conf = 95% confidence intervals on coeff
+% dof =  degrees of freedom used for confidence interval
+% err = standard error on coeff.
+function [coeff,conf,dof,err] = dcregress(x, y, dof, test_flag, plot_flag, ...
+                                          dof_flag)
 
     if ~exist('dof', 'var') | isempty(dof), dof = NaN; end
     if ~exist('test_flag', 'var'), test_flag = 0; end
     if ~exist('plot_flag', 'var'), plot_flag = 0; end
     if ~exist('err_flag', 'var'), err_flag = 1; end
+    if ~exist('dof_flag', 'var'), dof_flag = 1; end
 
     if test_flag
         test_dcregress;
@@ -39,11 +44,24 @@ function [coeff,conf,dof] = dcregress(x, y, dof, test_flag, plot_flag, ...
     P = ETEI * E' * var(res) * E * ETEI;
     err = sqrt(diag(P));  % standard error
 
-    if isnan(dof) & err_flag
-        % calculate degrees of freedom taking into account gaps
-        dof(1) = calcdof(xnan);
-        dof(2) = calcdof(ynan);
-        dof = min(dof-2);
+    % standard error of slope
+    n = length(x);
+    r = corrcoef(x,y); r = r(1,2);
+    syx = ((n-1)/(n-2) * var(y) * (1-r^2))^(1/2);
+    seb1 = syx/sqrt(var(x)*(n-1)); % should equal err(2)
+
+    assert((seb1-err(2))./err(2) < 5e-4);
+
+    if isnan(dof)
+        if dof_flag
+            % calculate degrees of freedom taking into account gaps
+            dof(1) = calcdof(xnan);
+            dof(2) = calcdof(ynan);
+            dof = min(dof-2);
+        else
+            % assume all points are statistically independent
+            dof = length(x)-2;
+        end
     end
 
     [lo,up] = conft(0.05, dof);
@@ -51,7 +69,7 @@ function [coeff,conf,dof] = dcregress(x, y, dof, test_flag, plot_flag, ...
     conf(2,1) = up * err(2);
 
     %%%%%%%%%%% use MATLAB regress
-    % [b, bint, r, rint, stats] = regress(y', E);
+    %[b, bint, r, rint, stats] = regress(y', E);
 
     % slope = b(2);
     % err = abs(bint(2) - b(2));
@@ -66,16 +84,32 @@ function [coeff,conf,dof] = dcregress(x, y, dof, test_flag, plot_flag, ...
 
     if plot_flag
         figure; hold on;
-        plot(x,y, '.', 'MarkerSize', 16, 'Color', [1 1 1]*0.4);
-        limx = xlim;
-        xvec = linspace(limx(1), limx(2), 1000);
-        yvec = slope * xvec + intercept;
-        yvechi = (slope + conf(2))*xvec + intercept + conf(1);
-        yveclo = (slope - conf(2))*xvec + intercept - conf(1);
-        plot(xvec, yvec, 'k');
-        plot(xvec, yvechi, 'k--');
-        plot(xvec, yveclo, 'k--');
+        plot_fit(x, y, coeff(2), conf(2), coeff(1), conf(1), 'k', 1);
+        rr = mf_wtls(x,y,0,0.4*std(y));
+        plot_fit(x, y, rr(1), rr(2), rr(3), rr(4), 'r', 0);
     end
+end
+
+function [] = plot_fit(x, y, slp, slperr, int, interr, color, plotxy)
+
+    if plotxy
+        plot(x,y, '.', 'MarkerSize', 16, 'Color', [1 1 1]*0.5);
+    end
+    limx = xlim;
+    xvec = linspace(limx(1), limx(2), 1000);
+    yvec = slp * xvec + int;
+    yvechi = (slp + slperr)*xvec + int + interr;
+    yveclo = (slp - slperr)*xvec + int - interr;
+
+    plot(xlim,xlim.*slp+int,'k--','LineWidth',2)
+    plot([0 max(xlim)],[0 max(xlim)].*(slp+slperr)+int+interr,'k--', ...
+         'Color', color, 'LineWidth',0.75)
+    plot([0 max(xlim)],[0 max(xlim)].*(slp-slperr)+int-interr,'k--', ...
+         'Color', color, 'LineWidth',0.75)
+    plot([min(xlim) 0],[min(xlim) 0].*(slp+slperr)+int-interr,'k--', ...
+         'Color', color, 'LineWidth',0.75)
+    plot([min(xlim) 0],[min(xlim) 0].*(slp-slperr)+int+interr,'k--', ...
+         'Color', color, 'LineWidth',0.75)
 end
 
 function [] = test_dcregress()
@@ -85,7 +119,7 @@ function [] = test_dcregress()
     x = linspace(0,1,8000);
     y = a*x + b + a/4 * rand(size(x));
 
-    [coef,conf] = dcregress(x,y);
+    [coef,conf] = dcregress(x,y, [], 0, 1);
     [coef-conf coef+conf]
     % [b, bint, r, rint, stats] = regress(y', [ones(size(x')) x']);
 end
