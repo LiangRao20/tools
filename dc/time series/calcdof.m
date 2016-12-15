74dcc03cce58bc362983eaed10c3fa0d532e585f
deepak <dpak.cherian@gmail.com>
2016-12-15 11:39:02 -0500

    Updated calcdof. Should be fixed.

--- dc/time series/calcdof.m
+++ dc/time series/calcdof.m
@@ -1,13 +1,22 @@
 % estimate degrees of freedom using integral timescale
-%        [dof, IntegralTimeScale] = calcdof(in)
-function [dof, IntegralTimeScale] = calcdof(in)
+%        [dof, IntegralTimeScale] = calcdof(in, filter, cutoff)
+% Set filter = 'narrowband' and cutoff to *band frequencies*
+% to use that methodology.
+function [dof, IntegralTimeScale] = calcdof(in, filter, cutoff)
+
+    if ~exist('filter', 'var')
+        filter = 'broadband';
+    end
 
     nans = isnan(in);
     edges = diff(nans);
     gapstart = find(edges == 1) + 1;
     gapend = find(edges == -1);
 
-    if isnan(in(1)), gapstart = [1 gapstart]; end
+    if isnan(in(1))
+        gapstart(2:end+1) = gapstart(1:end);
+        gapstart(1) = 1;
+    end
     if isnan(in(end))
         gapend(end+1) = length(in);
     else
@@ -40,28 +49,43 @@ function [dof, IntegralTimeScale] = calcdof(in)
 
         assert(all(isnan(in(range)) == 0), 'NaNs in selected range!');
 
-        % Using coef and biased are the same because I normalize by
-        % cmax later. Sarah Gille's notes uses biased, but says to
-        % normalize so that autocorrelation is 1 at 0 lag.
-        [c,lags] = xcorr(in(range) - mean(in(range)), 'coef');
-        [cmax,imax] = max(c);
+        if strcmpi(filter, 'narrowband')
+            if ~exist('cutoff', 'var')
+                error(['Provide filter cutoffs for narrowband time ' ...
+                       'series']);
+            end
 
-        if isempty(find(c < 0))
-            % if no zero-crossing in auto-correlation, then the
-            % segment isn't long enough to be meaningful
-            dof(ii) = NaN;
-            continue;
+            % length of segment
+            N = length(range);
+            % number of resolvable frequencies in segment
+            numfreq = length(min(cutoff):1/N:max(cutoff));
+            % amplitude and frequency of sinusoid at each frequency
+            % is number of degrees of freedom
+            dof(ii) = numfreq*2;
+        else
+            % Using coef and biased are the same because I normalize by
+            % cmax later. Sarah Gille's notes uses biased, but says to
+            % normalize so that autocorrelation is 1 at 0 lag.
+            [c,lags] = xcorr(in(range) - mean(in(range)), 'coef');
+            [cmax,imax] = max(c);
+
+            if isempty(find(c < 0))
+                % if no zero-crossing in auto-correlation, then the
+                % segment isn't long enough to be meaningful
+                dof(ii) = NaN;
+                continue;
+            end
+
+            % calculate a bunch of timescales and take maximum
+            % From Talley et al., Descriptive Physical Oceanography.
+            IntegralTimeScale = max(cumtrapz(c(imax:length(c))))./cmax;
+
+            % Techincally, for sin(2π/T t), IntegralTimeScale = T/2π
+            % The zero-crossing for that sinusoid is at T/4 points
+            % T/4 = IntegralTimeScale * π/2
+            % Sarah Gille's notes do not do this though. Kyun? Pata nahin.
+            dof(ii) = floor( length(in(range))/(IntegralTimeScale) );
         end
-
-        % calculate a bunch of timescales and take maximum
-        % From Talley et al., Descriptive Physical Oceanography.
-        IntegralTimeScale = max(cumtrapz(c(imax:length(c))))./cmax;
-
-        % Techincally, for sin(2π/T t), IntegralTimeScale = T/2π
-        % The zero-crossing for that sinusoid is at T/4 points
-        % T/4 = IntegralTimeScale * π/2
-        % Sarah Gille's notes do not do this though. Kyun? Pata nahin.
-        dof(ii) = floor( length(in(range)) / (IntegralTimeScale) );
     end
 
     dof = nansum(dof);
